345f788e7ba11b5f9125730f53bfce4e4140ba4d
Natalia Erazo <n.erazog@uniandes.edu.co>
2025-04-17 21:25:46 -0500

    Tutorial TypeScript

---
+++ tutorialTypescript/dataCourses.ts
@@ -0,0 +1,10 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Ingeniería de Sw", "Camilo", 3),
+  new Course("Sistemas transaccional", "Juan Pablo", 3),
+  new Course("TI en las organizaciones", "Liliana", 3),
+  new Course("El hombre y las plantas", "Santiago", 2),
+  new Course("Cálculo vectorial", "Juan Pablo", 3)
+] 
+  
