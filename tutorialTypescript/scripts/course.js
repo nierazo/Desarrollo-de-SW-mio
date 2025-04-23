fd3f7ebe599f13a7deff8398c5acaa0c60620cb8
Natalia Erazo <n.erazog@uniandes.edu.co>
2025-04-23 14:26:39 -0500

    finalizado

--- tutorialTypescript/scripts/course.js
+++
@@ -1,9 +0,0 @@
-var Course = /** @class */ (function () {
-    function Course(name, professor, credits) {
-        this.name = name;
-        this.credits = credits;
-        this.professor = professor;
-    }
-    return Course;
-}());
-export { Course };
